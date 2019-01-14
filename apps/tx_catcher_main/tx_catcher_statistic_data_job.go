4a1be918af030c49bf91d845c70e96000bd163ac
po-he <bluedg1486@gmail.com>
2019-01-14 10:28:50 +0000

    change

---
+++ apps/tx_catcher_main/tx_catcher_statistic_data_job.go
@@ -0,0 +1,30 @@
+package main
+
+import(
+	"github.com/FinDataCatcher/base"
+	//"github.com/FinDataCatcher/tx_catcher"
+	//"github.com/FinDataCatcher/utils"
+)
+
+type tx_catcher_statistic_data_job struct {}
+
+
+func (j tx_catcher_statistic_data_job) RequestData(ctx *base.FinDataCatcherJobContext)  (string, error)	{
+	bytes, err :=  base.DoHttpRequest(ctx.Url, 0)
+	if nil != err {
+		return "", err
+	}
+
+	return string(bytes), nil
+	//return fmt.Sprintf("http://stock.gtimg.cn/data/index.php?appn=detail&action=data&c=%v&p=%v", 1, 1), nil
+}
+	
+func (j tx_catcher_statistic_data_job) ProcessResponse(ctx *base.FinDataCatcherJobContext, resp string) bool {
+	//ctx
+	//info *DailyDownloadeInfo
+	
+	// download_info := ctx.DownloadInfo
+
+	// YY-MM-DD 前缀
+	return false
+}
---
+++ apps/tx_catcher_main/tx_catcher_tick_data_job.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/FinDataCatcher/base"
+	"github.com/FinDataCatcher/tx_catcher"
+	"github.com/FinDataCatcher/utils"
+	
+	//"github.com/syndtr/goleveldb/leveldb"
+)
+
+type tx_catcher_tick_data_job struct {}
+
+
+func (j tx_catcher_tick_data_job) RequestData(ctx *base.FinDataCatcherJobContext)  (string, error)	{
+	//return fmt.Sprintf("http://stock.gtimg.cn/data/index.php?appn=detail&action=data&c=%v&p=%v", 1), bkl
+	bytes, err :=  base.DoHttpRequest(ctx.Url, 0)
+	if nil != err {
+		return "", err
+	}
+
+	return string(bytes), nil
+}
+	
+func (j tx_catcher_tick_data_job) ProcessResponse(ctx *base.FinDataCatcherJobContext, resp string) bool {
+	//ctx
+	//info *DailyDownloadeInfo
+	download_info := ctx.DownloadInfo
+
+	// YY-MM-DD 前缀
+	date_str := utils.CurrentLocalDate()
+	ret, _, index, tick_list := tx_catcher.TX_parse_stock_tick_detail_response(resp, date_str)
+	if false == ret {
+		fmt.Printf("")
+		return false
+	}
+
+	// Tick數據保存到DB
+	for _, tick_data := range tick_list {
+		fmt.Printf("%v\n", tick_data)
+	}
+
+	// 記錄下一次索引
+	download_info.LastTickDataIndex = index
+	
+	// 保存一次
+	ctx.SaveDailyDownloadInfo()
+	return true
+}
